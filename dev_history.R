a0e4eb84f878377ad5abcb8cb7afc69acc124174
ggwinter <49496645+ggwinter@users.noreply.github.com>
2024-03-04 10:25:02 +0100

    Add GitHub links to DESCRIPTION

--- dev_history.R
+++ dev_history.R
@@ -1,6 +1,8 @@
 # available::available("rscribe") verifier si le nom du pacakge est disponible
 # taper  library(devtools)
 usethis::create_package("C:/Users/HP-G5/Documents/R/rscribe")
+usethis::use_git()
+usethis::use_github()
 # creer un fichier de suivi des modifications du package
 usethis::edit_file("dev_history.R")
 usethis::use_build_ignore('dev_history.R')
